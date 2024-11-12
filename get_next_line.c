c4421779c9a875c6df3cdd9834e605d3f01f031d
lmatkows <lmatkows@student.42perpignan.fr>
2024-11-12 14:40:04 +0100

    GNL en cours v2

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,15 @@
 /*   By: lmatkows <lmatkows@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 11:31:03 by lmatkows          #+#    #+#             */
-/*   Updated: 2024/11/12 14:28:18 by lmatkows         ###   ########.fr       */
+/*   Updated: 2024/11/12 14:39:12 by lmatkows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
 char	*get_next_line(int fd)
 {
-	int		len_ln;
+	int		ret;
 	char	*line;
 	int		i;
 
@@ -23,29 +22,31 @@ char	*get_next_line(int fd)
 	if (fd == -1)
 		return (NULL);
 	line = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-	while (line[i] != '\n')
+	while (ret != 0)
 	{
-		len_ln = read(fd, &line[i], sizeof(char));
-		printf("%c", line[i]);
+		ret = read(fd, &line[i], sizeof(char));
 		if ((line[i] == '\0') || (line[i] == '\n'))
 			return (line);
 		i ++;
-		//printf("%d\n", i);
 	}
+	if (ret == 0)
+		return (NULL);
 	line[i] = '\0';
 	return (line);
 }
-
+/*
+#include <stdio.h>
 
 int main (void)
 {
 	int		fd;
 
 	fd = open("Test.txt", O_RDONLY);
-	get_next_line(fd);
-	get_next_line(fd);
-	get_next_line(fd);
-	get_next_line(fd);
-	get_next_line(fd);
+	printf("%s",get_next_line(fd));
+	printf("%s",get_next_line(fd));
+	printf("%s",get_next_line(fd));
+	printf("%s",get_next_line(fd));
+	printf("%s",get_next_line(fd));
 	//printf("%s", get_next_line(fd));
 }
+*/
