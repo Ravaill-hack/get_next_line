f5dd903d989135f3234697190779766cf5d14db4
lmatkows <lmatkows@student.42perpignan.fr>
2024-11-13 10:27:13 +0100

    on recommence

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lmatkows <lmatkows@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 11:31:03 by lmatkows          #+#    #+#             */
-/*   Updated: 2024/11/12 18:07:16 by lmatkows         ###   ########.fr       */
+/*   Updated: 2024/11/13 10:26:01 by lmatkows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,45 +56,26 @@ char	*get_next_line(int fd)
 {
 	int			ind;
 	int			i;
-	static char	*tab;
-	static char	*line;
+	static char	*line = NULL;
+	static char *toomuchread = NULL;
+	char		*buffer;
+	char		*ret;
 
 	i = 0;
 	ind = -1;
 	line = NULL;
 	if (fd == -1)
 		return (NULL);
-	
-	while (ind != 0)
+	while ((ft_strchr(line, '\n') == 0) && (ind != 0))
 	{
-		line = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-		ind = read(fd, line, BUFFER_SIZE);
-		tab = ft_strjoin(tab, line);
-		free(line);
+		buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+		ind = read(fd, buffer, BUFFER_SIZE);
+		line = ft_strjoin(line, buffer);
+		//free(buffer);
 	}
-	
-	/*if (BUFFER_SIZE == 1)
-	{
-		while (ind != 0)
-		{
-			ind = read(fd, &tab[i], 1);
-			if ((tab[i] == '\0') || (tab[i] == '\n'))
-				return (line);
-			i ++;
-		}
-		if (ind == 0)
-		{
-			free (line);
-			return (NULL);
-		}
-		tab[i] = '\0';
-		return (line);	
-	}
-	else
-	{
-	tab =
-	}
-	*/
+	ret = ft_strjoin(toomuchread, substrto(line, '\n'));
+	toomuchread = susbtrfrom(line, '\n');
+	return (ret);
 }
 
 #include <stdio.h>
